837d1c019156c8dfc2561b14ce3cb28abe301bb8
mahadihassanriyadh <mahadi_hassan3931@yahoo.com>
2021-11-01 11:39:04 +0600

    done with this from module 61

--- src/Pages/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Firebase/firebase.config.js
@@ -1,12 +1,12 @@
 console.log(process.env);
 const firebaseConfig = {
-    apiKey: "AIzaSyAehlRrcQmoztMtJF4JePvNDfCEAZC_1MA",
-    authDomain: "genius-car-mechanics-d18d3.firebaseapp.com",
-    projectId: "genius-car-mechanics-d18d3",
-    storageBucket: "genius-car-mechanics-d18d3.appspot.com",
-    messagingSenderId: "924688078504",
-    appId: "1:924688078504:web:c904471d5d64a42410149d",
-    measurementId: "G-S2B5K8J41Q"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
   };
 
   
